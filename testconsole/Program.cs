337d5087a1c70213e685f10f47c0938724ef5295
Dispasim <91202903+Dispasim@users.noreply.github.com>
2023-04-25 13:14:13 +0200

    test console

---
+++ testconsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace testconsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
